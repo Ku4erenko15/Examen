4243a8b4849244ef4a14882aaabd69205d3b71a7
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2025-06-27 10:17:04 +0300

    Add files via upload

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateClass date1 = new DateClass(1987, 5, 22);
+
+            Console.WriteLine($"Текущая дата: {date1.Data}");
+            Console.WriteLine($"Пдыдущирей день: {date1.PreviousDay()}");
+            Console.WriteLine($"Следующий день: {date1.NextDay()}");
+
+            //Console.Write("Введите год: ");
+            //int year = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Введите месяц: ");
+            //int month = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Введите день: ");
+            //int day = Convert.ToInt32(Console.ReadLine());
+
+            //DateClass date = new DateClass(year, month, day);
+
+            //Console.WriteLine($"Текущая дата: {date.Data:dd.MM.yyyy}");
+            //Console.WriteLine($"Пдыдущирей день: {date.PreviousDay():dd.MM.yyyy}");
+            //Console.WriteLine($"Следующий день: {date.NextDay():dd.MM.yyyy}");
+
+            Console.ReadKey();
+        }
+    }
+}
+
